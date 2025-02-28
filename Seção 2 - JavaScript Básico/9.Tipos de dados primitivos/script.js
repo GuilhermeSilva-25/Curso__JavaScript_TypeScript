b110933e456f534ce2f8ac16a932beffb1c38114
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-02-28 20:42:16 -0300

    Estudo: tipos de dados primitivos

---
+++ Seção 2 - JavaScript Básico/9.Tipos de dados primitivos/script.js
@@ -0,0 +1,11 @@
+// String, number, undefined, null, boolean
+const nome = "Luiz"; // string
+const num = 10; // number
+let nomeAluno; // undefined ==> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo ==> não aponta pra local nenhum na memória
+const aprovado = false; // Boolean = true ou false (lógico)
+console.log(typeof aprovado);
+console.log(typeof nome);
+console.log(typeof num);
+console.log(typeof nomeAluno);
+console.log(typeof sobrenomeAluno);
