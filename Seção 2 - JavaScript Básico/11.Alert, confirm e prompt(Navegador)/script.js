50ab73488b7922e1ff28ee289cae05db46aeb406
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-03-02 20:26:32 -0300

    Estudo: alert, confirm e prompt

---
+++ Seção 2 - JavaScript Básico/11.Alert, confirm e prompt(Navegador)/script.js
@@ -0,0 +1,10 @@
+alert("Hello World!");
+const confirma = confirm("Tudo certo?");
+const nome = prompt("Digite o seu nome: ");
+let num = prompt("Digite um número: ");
+console.log(confirma);
+console.log(nome);
+console.log(typeof num);
+num = Number(num);
+console.log(typeof num);
+
---
+++ Seção 2 - JavaScript Básico/12.Exercício/script.js
@@ -0,0 +1,4 @@
+let num1 = Number(prompt("Digite o primeiro número: "));
+let num2 = Number(prompt("Digite o segundo número: "));
+let soma = num1 + num2;
+alert(`A soma de ${num1} e ${num2} é igual a ${soma}.`);
